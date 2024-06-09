a77685785396cbc5ffc4c870140cf8659b350ff2
Esraa <eamahmoud198@cit.just.edu.jo>
2024-06-09 16:18:57 +0300

    fix methods

--- CSU-Infra/Repository/LoginRepository.cs
+++ CSU-Infra/Repository/LoginRepository.cs
@@ -6,16 +6,16 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Microsoft.Extensions.Logging;
-using CSU_Core.Common; // Add this namespace for logging
+using CSU_Core.Common; 
 
 namespace CSU_Infra.Repository
 {
     public class LoginRepository : I_LoginRepository
     {
         private readonly IDbContext _dbContext;
-        private readonly ILogger<LoginRepository> _logger; // Inject ILogger
+        private readonly ILogger<LoginRepository> _logger; 
 
-        public LoginRepository(IDbContext dbContext, ILogger<LoginRepository> logger) // Add ILogger parameter to constructor
+        public LoginRepository(IDbContext dbContext, ILogger<LoginRepository> logger) 
         {
             this._dbContext = dbContext;
             this._logger = logger;
@@ -27,7 +27,7 @@ namespace CSU_Infra.Repository
             p.Add("p_email", login.Email, dbType: DbType.String, direction: ParameterDirection.Input);
             p.Add("p_password", login.Password, dbType: DbType.String, direction: ParameterDirection.Input);
 
-            IEnumerable<User> result = _dbContext.Connection.Query<User>("login_package.user_login", p, commandType: CommandType.StoredProcedure);
+            var result = _dbContext.Connection.Query<User>("login_package.user_login", p, commandType: CommandType.StoredProcedure);
 
             return result.FirstOrDefault();
        }
